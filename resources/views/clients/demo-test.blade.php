d4fa528509bfe36cb68f59f32aafcdee0ddb2eb5
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-03-08 16:10:57 +0700

    fisnish bai19

--- resources/views/clients/demo-test.blade.php
+++ resources/views/clients/demo-test.blade.php
@@ -1,3 +1,13 @@
 <h2>
     Demo View Codde
 </h2>
+@if (session('mess'))
+    <div class="alert alert-success">
+        {{ session('mess') }}
+    </div>
+@endif
+<form action="" method="POST">
+    @csrf
+    <input type="text" name="username" placeholder="Enter Name" value="{{ old('username') }}">
+    <button type="submit">Submit</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,37 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+Route::get('/', [HomeController::class, 'index'])->name('home');
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/san-pham', [HomeController::class, 'products'])->name('product');
 Route::get('/them-san-pham', [HomeController::class, 'getAdd']);
 // Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
 Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
+Route::get('/demo-response2', function (Request $request) {
+    return $request->cookie('Unicode', 'Training PHP');
+});
+
+Route::get('/lay-thong-tin', [HomeController::class, 'getArr']);
 Route::get('/demo-response', function () {
     // $content = json_decode(['Item1', 'Item2', 'Item3']);
     // $response = (new Response($content))->header('Content-Type', 'application/json');
     // return $response;
     // $response = (new Response())->cookie('Unicode', 'Trainning PhP');
     // return $response;
+    // $contentArr = ['name'=> 'Unicode',
+    //                 'version' => '10.x',
+    //                 'lesson' => 'HTTP response Laravel'   
+    //             ];
+    // return response()->json($contentArr, 404)->header("Api-Key",'1234');
+    // return view('clients.demo-test');
     return view('clients.demo-test');
-});
-Route::get('/demo-response2', function (Request $request) {
-    return $request->cookie('Unicode', 'Training PHP');
-});
 
+})->name('demo-response');
 
-Route::get('/lay-thong-tin', [HomeController::class, 'getArr']);
+Route::post('/demo-response', function (Request $request) {
+    if(!empty($request->username)){
+        return back()->withInput()->with('mess', 'Valida thanh cong');
+    }
+    return redirect(route('demo-response'))->with('mess', 'Valida ko thanh cong');
+});
