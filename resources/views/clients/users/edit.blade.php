1307faa3a10f24cc87158fbb1d7ef1ac73245b5d
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-03-13 20:30:46 +0700

    bai30

---
+++ resources/views/clients/users/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.client')
+@section('title')
+    {{ $title }}
+@endsection
+
+@section('content')
+    @if (session('msg'))
+        <div class="alert alert-success">{{ session('msg') }}</div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            Du lieu nhap vao khong hop le. Vui long kiem tra lai
+        </div>
+    @endif
+
+    <h1>{{ $title }}</h1>
+    <form action="{{ route('users.post-edit') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="fullname" class="form-label">Ho va ten</label>
+            <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Ho va ten"
+                value="{{ old('fullname') ?? $userDetail->fullname }}">
+            @error('fullname')
+                <span style="color: red">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control" id="email" name="email" placeholder="Email"
+                value="{{ old('email') ?? $userDetail->email }}">
+            @error('email')
+                <span style="color: red">{{ $message }}</span>
+            @enderror
+        </div>
+        <button class="btn btn-primary" type="submit">Update</button>
+        <a href="{{ route('users.index') }}" class="btn btn-warning">Quay lai</a>
+    </form>
+@endsection
--- resources/views/clients/users/lists.blade.php
+++ resources/views/clients/users/lists.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.client')
 @section('title')
-    {{$title}}
+    {{ $title }}
 @endsection
 
 @section('content')
     @if (session('msg'))
-        <div class="alert alert-success">{{session('msg')}}</div>
+        <div class="alert alert-success">{{ session('msg') }}</div>
     @endif
-    <h1>{{$title}}</h1>
-    <a href="{{route('users.add')}}" class="btn btn-primary">Them nguoi dung</a>
+    <h1>{{ $title }}</h1>
+    <a href="{{ route('users.add') }}" class="btn btn-primary">Them nguoi dung</a>
     <hr>
     <table class="table table-bordered">
         <thead>
@@ -17,24 +17,27 @@
                 <th>Name</th>
                 <th>Email</th>
                 <th>Time</th>
+                <th class="width:5%;">Edit</th>
+                <th class="width:5%;">Delete</th>
             </tr>
         </thead>
         <tbody>
             @if (!empty($userList))
-                 @foreach ($userList as $key => $item)
-                <tr>
-                    <td>{{$key+1}}</td>
-                    <td>{{$item->fullname}}</td>
-                    <td>{{$item->email}}</td>
-                    <td>{{$item->create_at}}</td>
-                </tr>
+                @foreach ($userList as $key => $item)
+                    <tr>
+                        <td>{{ $key + 1 }}</td>
+                        <td>{{ $item->fullname }}</td>
+                        <td>{{ $item->email }}</td>
+                        <td>{{ $item->create_at }}</td>
+                        <td><a href="{{ route('users.edit', $item->id) }}" class="btn btn-primary">Edit</a></td>
+                        {{-- <td><a href="{{route('users.delete', $item->id)}}" class="btn btn-danger">Delete</a></td> --}}
+                    </tr>
                 @endforeach
             @else
                 <tr>
-                    <td colspan="4">No user</td>
+                    <td colspan="6">No user</td>
                 </tr>
             @endif
         </tbody>
     </table>
 @endsection
-
