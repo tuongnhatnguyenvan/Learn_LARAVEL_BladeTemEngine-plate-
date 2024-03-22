301284ea398c8b97c90709a9f27d6a49151f310d
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-03-22 09:59:46 +0700

    fisnish bai36

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -51,16 +51,16 @@ class Users extends Model
         // $id = 2;
         // $list = DB::table($this->table)
         //     ->select('fullname as hoten', 'email', 'id', 'update_at','create_at')
-            // ->where('id', 'like', '%1%')
-            // ->whereNotIn('id', [1, 3])
-            // ->whereIn('id', [1, 3])
-            // ->whereNotBetween('id', [1, 3])
-            // ->whereBetween('id', [1, 3])
-            // ->whereNull('update_at')    
-            // ->whereNotNull('update_at')    
-            // ->whereYear('create_at', '2024')
-            // ->whereColumn('create_at', 'update_at') 
-            // ->get();
+        // ->where('id', 'like', '%1%')
+        // ->whereNotIn('id', [1, 3])
+        // ->whereIn('id', [1, 3])
+        // ->whereNotBetween('id', [1, 3])
+        // ->whereBetween('id', [1, 3])
+        // ->whereNull('update_at')    
+        // ->whereNotNull('update_at')    
+        // ->whereYear('create_at', '2024')
+        // ->whereColumn('create_at', 'update_at') 
+        // ->get();
         // ->where('id', '>', 1)   
         // ->where('id', '<>', 8)
         // ->where('id', '>=', 8)
@@ -79,24 +79,64 @@ class Users extends Model
         // ->where(function ($query) use ($id) {
         //     $query->where('id', '<', $id)->orWhere('id', '>', $id);
         // })
-        
+
         //Join bang
-        $list = DB::table('users')
+        // $list = DB::table('users')
         // ->select('users.*', 'groups.name as group_name')
         // ->rightJoin('groups','users.group_id','=','groups.id')
         // ->orderBy('create_at', 'desc')
         // ->orderBy('id', 'desc')
         // ->inRandomOrder()
-        ->select(DB::raw('count(id) as email_count'), 'email', 'fullname')
-        ->groupBy('email')  
+        // ->select(DB::raw('count(id) as email_count'), 'email', 'fullname')
+        // ->groupBy('email')  
         // ->having('email_count', '>=', 1)
-        ->groupBy('fullname')
+        // ->groupBy('fullname')
         // ->limit(1)
         // ->offset(1)
-        ->take(2)
-        ->skip(2)
-        ->get();
-        dd($list);
+        // ->take(2)
+        // ->skip(2)
+        // ->get();
+
+        // insert data
+        // $status = DB::table('users')->insert([
+        //     'fullname' => 'Nguyễn Văn Tường Ne',
+        //     'email' => 'ne.nguyenvan25@student.passerellesnumeriques.org',
+        //     'group_id' => 1,
+        //     'create_at' => date('Y-m-d H:i:s'),
+        // ]);
+
+        // $lastId = DB::getPdo()->lastInsertId();
+        // dd($lastId);
+
+        // $lastId = DB::table('users')->insertGetId([
+        //     'fullname' => 'Nguyễn Văn Tường Non',
+        //     'email' => 'non.nguyenvan25@student.passerellesnumeriques.org',
+        //     'group_id' => 1,
+        //     'create_at' => date('Y-m-d H:i:s'),
+        // ]);
+        // dd($lastId);
+
+        // sua bang ghi
+        // $status = DB::table('users')
+        // ->where('id', 6)
+        // ->update([
+        //     'fullname' => 'Nguyễn Văn Tường Nen',
+        //     'email' => 'nen.nguyenvan25@student.passerellesnumeriques.org',
+        //     'update_at' => date('Y-m-d H:i:s'),
+        // ]);
+
+        //xoa bang ghi
+        // $status = DB::table('users')
+        // ->where('id', 6)
+        // ->delete();
+
+        //dem bang ghi
+        $count = DB::table('users')
+        ->where('id','>',3)
+        ->count();
+
+        dd($count);
+
         $sql = DB::getQueryLog();
         // dd($sql);
         $detail = DB::table($this->table)->first();
